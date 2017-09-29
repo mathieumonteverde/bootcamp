ed58b2e119eb04eb726df53cd8ebbff0ced8a1cd
Viking & Robot <vikingandrobot@gmail.com>
2017-09-29 15:05:33 +0200

    Added Dummy description to Dummy class

--- amt_bootcamp_java/src/main/java/com/heig/amt_bootcamp_java/services/Dummy.java
+++ amt_bootcamp_java/src/main/java/com/heig/amt_bootcamp_java/services/Dummy.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.heig.amt_bootcamp_java.services;
 
 /**
  *
- * @author mathieu
+ * @author Mathieu Monteverde
+ * 
+ * Just a dummy class for git to consider this package...
  */
 public class Dummy {
    
