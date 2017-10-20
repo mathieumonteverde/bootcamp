0d3aabb23420c89d1a665f236ec3f5a034a85fa3
Viking & Robot <vikingandrobot@gmail.com>
2017-10-20 15:49:43 +0200

    Working pagination

--- amt_bootcamp_java/src/main/java/com/heig/amt_bootcamp_java/web/PokemonServlet.java
+++ amt_bootcamp_java/src/main/java/com/heig/amt_bootcamp_java/web/PokemonServlet.java
@@ -20,12 +20,12 @@ import javax.servlet.http.HttpServletResponse;
  * @author mathieu
  */
 public class PokemonServlet extends HttpServlet {
-   
+
    public static final int POKEMONS_PER_PAGE = 5;
 
    @EJB
    private PokemonsManagerLocal pokemonsManager;
-   
+
    /**
     * Handles the HTTP <code>GET</code> method.
     *
@@ -36,62 +36,75 @@ public class PokemonServlet extends HttpServlet {
     */
    @Override
    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-           throws ServletException, IOException 
-   {
+           throws ServletException, IOException {
 
       // Get page number
       int page = 1;
       int pokemonsPerPage = POKEMONS_PER_PAGE;
       try {
          page = Integer.parseInt(request.getParameter("page"));
-      } catch(NumberFormatException e) {
+      } catch (NumberFormatException e) {
          page = 1; // TODO: 404 or error ?
       }
-      
+
       try {
          pokemonsPerPage = Integer.parseInt(request.getParameter("pokemonsPerPage"));
-      } catch(NumberFormatException e) {
+      } catch (NumberFormatException e) {
          pokemonsPerPage = POKEMONS_PER_PAGE; // TODO: 404 or error ?
       }
-      
+
       int maxNbPokemon = pokemonsManager.count();
-      int maxNbPage = (int) Math.ceil(maxNbPokemon / (double)pokemonsPerPage);
-      
+      int maxNbPage = (int) Math.ceil(maxNbPokemon / (double) pokemonsPerPage);
+
       // Page limitation
-      if(page < 1 || page > maxNbPage) {
-         // TODO : Display an error message or 404 page
+      if (page < 1) {
+         page = 1;
       }
-      
+      if (page > maxNbPage) {
+         page = maxNbPage;
+      }
+
       response.setContentType("text/html;charset=UTF-8");
-      request.setAttribute("pokemons", pokemonsManager.findAll(pokemonsPerPage, (page-1) * pokemonsPerPage));
-      
+      request.setAttribute("pokemons", pokemonsManager.findAll(pokemonsPerPage, (page - 1) * pokemonsPerPage));
+
       // Pagination variables
       request.setAttribute("page", page);
       request.setAttribute("pokemonsPerPage", pokemonsPerPage);
       request.setAttribute("maxNbPage", maxNbPage);
-      
-      List pagesToDisplay = new LinkedList();
-      // If possible, add at most the three first pages
-      int pageCount = 1;
-      while (pageCount <= 3 && pageCount <= maxNbPage) {
-         pagesToDisplay.add(pageCount++);
-      }
-      
-      // Then add the current page
-      if (pageCount < page) {
-         pagesToDisplay.add(page);
-         pageCount = page;
-      }
-      
-      // Then if possible, add the last three pages
-      if (pageCount < maxNbPage) {
-         pageCount = maxNbPage - pageCount < 3 ? maxNbPage - pageCount : maxNbPage - 2;
-         while (pageCount <= maxNbPage) {
-            pagesToDisplay.add(pageCount++);
+
+      // Pagination creation
+      List beforePagination = new LinkedList();
+      List pagePagination = new LinkedList();
+      List afterPagination = new LinkedList();
+
+      // If there is a small number of pages, create a full pagination
+      if (maxNbPage <= 6) {
+         for (int i = 1; i <= maxNbPage; ++i) {
+            pagePagination.add(i);
+         }
+      } else {
+         // Else parse it in three
+         // If possible, add at most the three first pages
+         int pageCount = 1;
+         if (pageCount < page - 2) {
+            beforePagination.add(pageCount);
+         }
+
+         pageCount = page - 2 < 1 ? 1 : page - 2;
+         while (pageCount <= page + 2 && pageCount <= maxNbPage) {
+            pagePagination.add(pageCount++);
+         }
+
+         pageCount = (maxNbPage > page + 3) ? maxNbPage : page + 3;
+         if (pageCount <= maxNbPage) {
+            afterPagination.add(pageCount);
          }
       }
-      request.setAttribute("pagesToDisplay", pagesToDisplay);
-      
+
+      request.setAttribute("beforePagination", beforePagination);
+      request.setAttribute("pagePagination", pagePagination);
+      request.setAttribute("afterPagination", afterPagination);
+
       request.getRequestDispatcher("/WEB-INF/views/pokemons.jsp").forward(request, response);
    }
 
